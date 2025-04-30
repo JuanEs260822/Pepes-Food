ddff4e4c93e81a81fd6d93af470cbeaa48d3e21c
JuanEs260822 <sotogarciajuanesteban2@gmail.com>
2025-04-29 18:09:20 -0600

    cambio1

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,13 @@
 const visibilidad = document.querySelector('.visibilidad');
+const tipo_input = document.getElementById('password1');
 visibilidad.addEventListener("click", () => {
   if (visibilidad.classList.contains('fa-eye-slash')) {
     visibilidad.classList.remove('fa-eye-slash');
     visibilidad.classList.add('fa-eye');
+    tipo_input.type = "password";
   } else {
     visibilidad.classList.remove('fa-eye');
     visibilidad.classList.add('fa-eye-slash');
+    tipo_input.type = "text";
   }
 });
